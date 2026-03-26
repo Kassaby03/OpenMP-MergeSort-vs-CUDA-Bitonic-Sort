13191970b7682b44b31b79edb699d3a7a08aab3d
mohamedtoukhy03 <mohamedtd03@gmail.com>
2026-03-26 07:38:26 +0200

    replace std::sort with recursive merge sort

--- src/sort/serial_sort.cpp
+++ src/sort/serial_sort.cpp
@@ -1,8 +1,49 @@
 #include "sort/serial_sort.h"
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+void serialMergeArrays(vector<int>& arr, vector<int>& temp, long left, long mid, long right) {
+    long i = left;
+    long j = mid + 1;
+    long k = left;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+
+    for (long idx = left; idx <= right; idx++) {
+        arr[idx] = temp[idx];
+    }
+}
+
+void serialMergeSortRec(vector<int>& arr, vector<int>& temp, long left, long right) {
+    if (left >= right) return;
+
+    long mid = left + (right - left) / 2;
+
+    serialMergeSortRec(arr, temp, left, mid);
+    serialMergeSortRec(arr, temp, mid + 1, right);
+
+    serialMergeArrays(arr, temp, left, mid, right);
+}
+
 void serialSort(vector<int>& arr) {
-    sort(arr.begin(), arr.end());
+    long arrSize = arr.size();
+    if (arrSize < 2) return;
+    
+    vector<int> temp(arrSize);
+    serialMergeSortRec(arr, temp, 0, arrSize - 1);
 }
